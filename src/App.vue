cd31b99ae6208e419e98996c56f8ff64618092ad
hooray <304327508@qq.com>
2021-09-04 14:11:39 +0800

    用 script setup 语法糖重写

--- src/App.vue
+++ src/App.vue
@@ -4,41 +4,33 @@
     </div>
 </template>
 
-<script>
-export default {
-    provide() {
-        return {
-            reload: this.reload
-        }
-    },
-    data() {
-        return {
-            isRouterAlive: true
-        }
-    },
-    watch: {
-        $route: 'routeChange',
-        '$store.state.settings.title': {
-            handler() {
-                this.setDocumentTitle()
-            },
-            immediate: true
-        }
-    },
-    methods: {
-        reload() {
-            this.isRouterAlive = false
-            this.$nextTick(() => (this.isRouterAlive = true))
-        },
-        routeChange(newVal, oldVal) {
-            if (newVal.name == oldVal.name) {
-                this.reload()
-            }
-        },
-        setDocumentTitle() {
-            let title = this.$store.state.settings.title
-            document.title = `${title} - ${import.meta.env.VITE_APP_TITLE}`
-        }
+<script setup>
+import { provide, ref, watch, nextTick } from 'vue'
+import { useStore } from 'vuex'
+import { useRoute } from 'vue-router'
+
+const store = useStore()
+const route = useRoute()
+const isRouterAlive = ref(true)
+
+provide('reload', reload)
+function reload() {
+    isRouterAlive.value = false
+    nextTick(() => (isRouterAlive.value = true))
+}
+
+watch(() => route, (newVal, oldVal) => routeChange(newVal, oldVal))
+function routeChange(newVal, oldVal) {
+    store.commit('settings/setTitle', route.meta.title)
+    if (newVal.name == oldVal.name) {
+        reload()
     }
 }
+
+watch(() => store.state.settings.title, () => {
+    let title = store.state.settings.title
+    document.title = title ? `${title} - ${import.meta.env.VITE_APP_TITLE}` : import.meta.env.VITE_APP_TITLE
+}, {
+    immediate: true
+})
 </script>
