655af8e7cc951a324ea7a29010ad83d48574b372
Hooray <304327508@qq.com>
2022-05-25 16:10:19 +0800

    修改入口文件文案

--- src/views/index.vue
+++ src/views/index.vue
@@ -8,6 +8,6 @@
 
 <template>
     <div>
-        欢迎使用 Fantastic-template ，点击进入<RouterLink to="/example/svgicon">演示Demo</RouterLink>
+        欢迎使用 Fantastic-startkit ，点击进入<RouterLink to="/example/svgicon">演示Demo</RouterLink>
     </div>
 </template>
