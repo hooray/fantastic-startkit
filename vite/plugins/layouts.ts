b44e7de6d4cdb37595c98ef22f881b3ed783de23
hooray <304327508@qq.com>
2022-08-19 20:37:55 +0800

    更新依赖

--- vite/plugins/layouts.ts
+++ vite/plugins/layouts.ts
@@ -1,8 +1,5 @@
 import Layouts from 'vite-plugin-vue-layouts'
 
 export default function createLayouts() {
-  return Layouts({
-    layoutsDirs: 'src/layout',
-    defaultLayout: 'index',
-  })
+  return Layouts()
 }
