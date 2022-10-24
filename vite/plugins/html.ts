7f02182592935a341302a59486223fbc4549bd0a
hooray <304327508@qq.com>
2022-10-25 02:08:25 +0800

    调整 copyright 信息和展示逻辑

--- vite/plugins/html.ts
+++ vite/plugins/html.ts
@@ -1,20 +1,22 @@
 import { createHtmlPlugin } from 'vite-plugin-html'
 
-const copyright_common_style = 'font-size: 14px; margin-bottom: 2px; padding: 6px 8px; color: #fff;'
-const copyright_main_style = `${copyright_common_style} background: #e24329;`
-const copyright_sub_style = `${copyright_common_style} background: #707070;`
-
 export default function createHtml(env, isBuild) {
   const { VITE_APP_TITLE, VITE_APP_DEBUG_TOOL } = env
-  const copyrightScript = `
+  let copyrightScript = ''
+  if (isBuild) {
+    const copyright_common_style = 'font-size: 14px; margin-bottom: 2px; padding: 6px 8px; color: #fff;'
+    const copyright_main_style = `${copyright_common_style} background: #e24329;`
+    const copyright_sub_style = `${copyright_common_style} background: #707070;`
+    copyrightScript += `
 <script>
 if ((navigator.language || navigator.browserLanguage).toLowerCase() === 'zh-cn') {
-  console.info('%c由%cFantastic-startkit%c提供技术支持', '${copyright_sub_style}', '${copyright_main_style}', '${copyright_sub_style}', '\\nhttps://hooray.gitee.io/fantastic-startkit/');
+  console.info('%c由%cFantastic-startkit%c驱动', '${copyright_sub_style}', '${copyright_main_style}', '${copyright_sub_style}', '\\nhttps://hooray.gitee.io/fantastic-startkit/');
 } else {
   console.info('%cPowered by%cFantastic-startkit', '${copyright_sub_style}', '${copyright_main_style}', '\\nhttps://hooray.github.io/fantastic-startkit/');
 }
 </script>
-  `
+    `
+  }
   let devtoolScript = ''
   switch (VITE_APP_DEBUG_TOOL) {
     case 'eruda':
@@ -34,7 +36,7 @@ if ((navigator.language || navigator.browserLanguage).toLowerCase() === 'zh-cn')
       `
       break
   }
-  const html = createHtmlPlugin({
+  return createHtmlPlugin({
     inject: {
       data: {
         title: VITE_APP_TITLE,
@@ -44,5 +46,4 @@ if ((navigator.language || navigator.browserLanguage).toLowerCase() === 'zh-cn')
     },
     minify: isBuild,
   })
-  return html
 }
