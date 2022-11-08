490b982756c62bfaecf838ebf406a1185b4fec62
hooray <304327508@qq.com>
2022-11-08 16:25:40 +0800

    postcss-px-to-viewport 替换为 postcss-px2vp

--- postcss.config.js
+++ postcss.config.js
@@ -1,26 +1,28 @@
 module.exports = {
   plugins: {
-    // 'postcss-px-to-viewport': {
-    //   // 需要转换的单位，默认为 px
+    // 'postcss-px2vp': {
+    //   // 需要转换的单位，默认为"px"
     //   unitToConvert: 'px',
-    //   // 视窗的宽度，对应的是我们设计稿的宽度
-    //   viewportWidth: 750,
-    //   // 指定 px 转换为视窗单位值的小数位数（很多时候无法整除）
-    //   unitPrecision: 3,
+    //   // 设计稿的视口宽度
+    //   viewportWidth: 320,
+    //   // 单位转换后保留的精度
+    //   unitPrecision: 5,
     //   // 能转化为 vw 的属性列表
     //   propList: ['*'],
-    //   // 指定需要转换成的视窗单位，建议使用 vw
+    //   // 希望使用的视口单位
     //   viewportUnit: 'vw',
     //   // 字体使用的视口单位
     //   fontViewportUnit: 'vw',
-    //   // 指定不转换为视窗单位的类，可以自定义，可以无限添加,建议定义一至两个通用的类名
-    //   selectorBlackList: [
-    //     '.ignore',
-    //   ],
-    //   // 小于或等于 1px 不转换为视窗单位，你也可以设置为你想要的值
+    //   // 需要忽略的 CSS 选择器，不会转为视口单位，使用原有的 px 等单位
+    //   selectorBlackList: [],
+    //   // 设置最小的转换数值，如果为 1 的话，只有大于 1 的值会被转换
     //   minPixelValue: 1,
-    //   // 允许在媒体查询中转换 px
+    //   // 媒体查询里的单位是否需要转换单位
     //   mediaQuery: false,
+    //   // 是否直接更换属性值，而不添加备用属性
+    //   replace: true,
+    //   // 忽略某些文件夹下的文件或特定文件，例如 'node_modules' 下的文件
+    //   exclude: [],
     // },
     autoprefixer: {},
   },
