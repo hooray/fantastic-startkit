989056f5efd8c1dd1a4fa627fd326ac262930da8
hooray <304327508@qq.com>
2023-02-13 16:57:38 +0800

    更新依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import fs from 'fs'
-import path from 'path'
+import fs from 'node:fs'
+import path from 'node:path'
 import { defineConfig, loadEnv } from 'vite'
 import createVitePlugins from './vite/plugins'
 
@@ -9,7 +9,9 @@ export default ({ mode, command }) => {
   // 全局 scss 资源
   const scssResources = []
   fs.readdirSync('src/assets/styles/resources').forEach((dirname) => {
-    if (fs.statSync(`src/assets/styles/resources/${dirname}`).isFile()) { scssResources.push(`@import "src/assets/styles/resources/${dirname}";`) }
+    if (fs.statSync(`src/assets/styles/resources/${dirname}`).isFile()) {
+      scssResources.push(`@import "src/assets/styles/resources/${dirname}";`)
+    }
   })
   // css 精灵图相关
   fs.readdirSync('src/assets/sprites').forEach((dirname) => {
