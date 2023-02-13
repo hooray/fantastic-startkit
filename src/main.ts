20d8d40fa62fc9c20ef5f7f01c5b42b4ab845930
hooray <304327508@qq.com>
2023-02-13 16:58:09 +0800

    更新依赖

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import { createApp } from 'vue'
 import App from './App.vue'
 import pinia from './store'
 import router from './router'
--- vite/plugins/components.ts
+++ vite/plugins/components.ts
@@ -3,6 +3,7 @@ import components from 'unplugin-vue-components/vite'
 export default function createComponents() {
   return components({
     dirs: ['src/components'],
+    include: [/\.vue$/, /\.vue\?vue/, /\.tsx$/],
     dts: './src/types/components.d.ts',
   })
 }
