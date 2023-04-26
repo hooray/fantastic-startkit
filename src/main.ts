4cf27c60f0aa81741c7bf3b3774e61bef3829b8b
Hooray Hu <304327508@qq.com>
2023-04-26 23:05:14 +0800

    更新依赖

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import './utils/system.copyright'
 import App from './App.vue'
 import pinia from './store'
 import router from './router'
+
 // 加载 svg 图标
 import 'virtual:svg-icons-register'
+
 // 全局样式
 import '@/assets/styles/globals.scss'
 
