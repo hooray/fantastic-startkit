6017879446b755bcf9f8f4af695e83d2871ddbfc
Hooray Hu <304327508@qq.com>
2023-07-15 10:27:51 +0800

    更新依赖

--- src/types/shims.d.ts
+++ src/types/shims.d.ts
@@ -4,12 +4,6 @@ declare interface Window {
   // extend the window
 }
 
-declare module '*.vue' {
-  import { type DefineComponent } from 'vue'
-
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
 declare module 'vue-router' {
   interface RouteMeta {
     title?: string
