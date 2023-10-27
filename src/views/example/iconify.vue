40b297724574a3fa27933d2c6b5c32ad83c291b0
Hooray Hu <304327508@qq.com>
2023-10-27 15:11:09 +0800

    更新依赖

--- src/views/example/iconify.vue
+++ src/views/example/iconify.vue
@@ -7,13 +7,13 @@ meta:
 <template>
   <div>
     <p>Iconify 图标</p>
-    <svg-icon name="logos:vue" class="text-[48px]" />
-    <svg-icon name="logos:vitejs" class="text-[48px]" />
-    <svg-icon name="uim:box" class="text-[48px]" />
+    <SvgIcon name="logos:vue" class="text-[48px]" />
+    <SvgIcon name="logos:vitejs" class="text-[48px]" />
+    <SvgIcon name="uim:box" class="text-[48px]" />
     <p>搭配 unocss 使用，将不会渲染成 svg 标签</p>
-    <svg-icon name="i-logos:vue" class="text-[48px]" />
-    <svg-icon name="i-logos:vitejs" class="text-[48px]" />
-    <svg-icon name="i-uim:box" class="text-[48px]" />
+    <SvgIcon name="i-logos:vue" class="text-[48px]" />
+    <SvgIcon name="i-logos:vitejs" class="text-[48px]" />
+    <SvgIcon name="i-uim:box" class="text-[48px]" />
     <p>使用方法：</p>
     <ol>
       <li>上 <a href="https://icon-sets.iconify.design/" target="_blank">Iconify</a> 找寻需要的图标</li>
--- src/views/example/svgicon.vue
+++ src/views/example/svgicon.vue
@@ -7,8 +7,8 @@ meta:
 <template>
   <div>
     <p>这是两个 Svg Icon 图标</p>
-    <svg-icon name="example" class="example-icon" />
-    <svg-icon name="example.color" class="example-icon" />
+    <SvgIcon name="example" class="example-icon" />
+    <SvgIcon name="example.color" class="example-icon" />
     <p>使用方法：</p>
     <ol>
       <li>上 <a href="https://www.iconfont.cn/" target="_blank">Iconfont</a> 下载需要的 svg 图标</li>
