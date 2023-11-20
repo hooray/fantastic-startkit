baf1725d20f8b429df573d9052f5ffd5b53178d1
Hooray Hu <304327508@qq.com>
2023-11-20 10:16:49 +0800

    plop相关配置改用ESM写法

--- plopfile.js
+++ plopfile.js
@@ -1,6 +1,13 @@
-module.exports = function (plop) {
+import { promises as fs } from 'node:fs'
+
+export default async function (plop) {
   plop.setWelcomeMessage('请选择需要创建的模式：')
-  plop.setGenerator('page', require('./plop-templates/page/prompt'))
-  plop.setGenerator('component', require('./plop-templates/component/prompt'))
-  plop.setGenerator('store', require('./plop-templates/store/prompt'))
+  const items = await fs.readdir('./plop-templates')
+  for (const item of items) {
+    const stat = await fs.lstat(`./plop-templates/${item}`)
+    if (stat.isDirectory()) {
+      const prompt = await import(`./plop-templates/${item}/prompt.js`)
+      plop.setGenerator(item, prompt.default)
+    }
+  }
 }
