f4d1a58fd3b664ce852e7d7ba3b90db6b1c1c30d
Hooray Hu <304327508@qq.com>
2024-03-14 10:17:49 +0800

    更新依赖

--- src/views/index.vue
+++ src/views/index.vue
@@ -4,10 +4,12 @@ meta:
 </route>
 
 <template>
-  <div>
-    欢迎使用 Fantastic-startkit ，点击进入
-    <RouterLink to="/example/svgicon">
-      演示Demo
+  <div class="flex-col-center">
+    <p>
+      <a rel="noreferrer" href="https://hooray.github.io/fantastic-startkit/" target="_blank">Fantastic-startkit</a>
+    </p>
+    <RouterLink to="/example" class="inline-block cursor-pointer rounded bg-teal-700 px-4 py-1 text-white no-underline disabled:cursor-default disabled:bg-gray-600 hover:bg-teal-800 disabled:opacity-50 !outline-none">
+      演示 demo
     </RouterLink>
   </div>
 </template>
