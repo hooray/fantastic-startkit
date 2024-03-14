0d9e0d56f2c37870fd328e6bf30ed63eb747c08d
Hooray Hu <304327508@qq.com>
2024-03-14 10:13:58 +0800

    更新依赖

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,10 +1,12 @@
 <route lang="yaml">
-meta:
-  layout: false
-</route>
+  meta:
+    layout: false
+  </route>
 
 <template>
-  <div>
-    欢迎使用 Fantastic-startkit !
+  <div class="flex-col-center">
+    <p>
+      <a rel="noreferrer" href="https://hooray.github.io/fantastic-startkit/" target="_blank">Fantastic-startkit</a>
+    </p>
   </div>
 </template>
