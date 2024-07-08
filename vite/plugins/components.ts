07868e9f740f2cb418e1a157ec3fe2aef24116f9
Hooray Hu <304327508@qq.com>
2024-07-08 16:07:16 +0800

    vite插件合并到单个文件中维护

--- vite/plugins/components.ts
+++
@@ -1,9 +0,0 @@
-import components from 'unplugin-vue-components/vite'
-
-export default function createComponents() {
-  return components({
-    dirs: ['src/components'],
-    include: [/\.vue$/, /\.vue\?vue/, /\.tsx$/],
-    dts: './src/types/components.d.ts',
-  })
-}
