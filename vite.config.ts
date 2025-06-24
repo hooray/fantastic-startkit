aaaab164539999c052b39dfd5f748df638eb67f0
Hooray <304327508@qq.com>
2025-06-24 23:20:03 +0800

    更新依赖

--- vite.config.ts
+++ vite.config.ts
@@ -42,7 +42,6 @@ export default defineConfig(({ mode, command }) => {
     css: {
       preprocessorOptions: {
         scss: {
-          api: 'modern-compiler',
           additionalData: scssResources.join(''),
         },
       },
