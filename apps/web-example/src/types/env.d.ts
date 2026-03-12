deaa3201b5cbeabb02deea2eedfd71898eeb939b
Hooray <304327508@qq.com>
2026-03-13 03:16:01 +0800

    更新依赖

--- apps/web-example/src/types/env.d.ts
+++ apps/web-example/src/types/env.d.ts
@@ -2,10 +2,6 @@
 interface ImportMetaEnv {
   // Auto generate by env-parse
   /**
-   * 页面标题
-   */
-  readonly VITE_APP_TITLE: string
-  /**
    * 接口请求地址，会设置到 axios 的 baseURL 参数上
    */
   readonly VITE_APP_API_BASEURL: string
@@ -14,6 +10,14 @@ interface ImportMetaEnv {
    */
   readonly VITE_APP_DEBUG_TOOL: string
   /**
+   * 页面标题
+   */
+  readonly VITE_APP_TITLE: string
+  /**
+   * 是否启用 console 工具
+   */
+  readonly VITE_ENABLE_CONSOLE: boolean
+  /**
    * 是否开启代理
    */
   readonly VITE_ENABLE_PROXY: boolean
@@ -21,10 +25,6 @@ interface ImportMetaEnv {
    * 是否启用 Vue 开发工具
    */
   readonly VITE_ENABLE_VUE_DEVTOOLS: boolean
-  /**
-   * 是否启用 console 工具
-   */
-  readonly VITE_ENABLE_CONSOLE: boolean
   /**
    * 启动编辑器，该配置用于 vite-plugin-vue-devtools 和 unplugin-turbo-console
    * 支持IDE列表 https://github.com/yyx990803/launch-editor#supported-editors
