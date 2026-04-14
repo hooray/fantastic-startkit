db797e911f741e1cf97aa4e73acbb48b1f25dc85
Hooray <304327508@qq.com>
2026-04-14 20:36:48 +0800

    优化错误处理逻辑，更新响应数据格式说明

--- apps/core/src/api/index.ts
+++ apps/core/src/api/index.ts
@@ -48,17 +48,10 @@ api.interceptors.request.use(
 function handleError(error: any) {
   if (error.status === 401) {
     useUserStore().logout()
-    throw error
   }
-  let message = error.message
-  if (message === 'Network Error') {
-    message = '后端网络故障'
-  }
-  else if (message.includes('timeout')) {
-    message = '接口请求超时'
-  }
-  else if (message.includes('Request failed with status code')) {
-    message = `接口${message.substr(message.length - 3)}异常`
+  else {
+    // 这里做错误提示，如果使用了 element plus 则可以使用 Message 进行提示
+    // Message.error(error.message)
   }
   return Promise.reject(error)
 }
@@ -67,45 +60,45 @@ api.interceptors.response.use(
   (response) => {
     /**
      * 全局拦截请求发送后返回的数据，如果数据有报错则在这做全局的错误提示
-     * 假设返回数据格式为：{ status: 1, error: '', data: '' }
-     * 规则是当 status 为 1 时表示请求成功，为 0 时表示接口需要登录或者登录状态失效，需要重新登录
-     * 请求出错时 error 会返回错误信息
+     * 约定的数据格式：{ status: 1 | 0, error: string, data: object }
+     * status 只有两种状态，1 表示请求成功，0 表示接口需要登录或者登录状态失效，需要重新登录
+     * error 只有在请求出错时才会有值，表示错误信息
+     * data 只有在请求成功时才会有值，表示请求返回的数据
      */
-    if (response.data.status === 1) {
-      if (response.data.error !== '') {
-        // 这里做错误提示，如果使用了 element plus 则可以使用 Message 进行提示
-        // Message.error(options)
-        return Promise.reject(response.data)
+    if (typeof response.data === 'object') {
+      if (response.data.status === 1) {
+        if (response.data.error) {
+          // 这里做错误提示，如果使用了 element plus 则可以使用 Message 进行提示
+          // Message.error(response.data.error)
+          return Promise.reject(response.data)
+        }
       }
+      else {
+        useUserStore().logout()
+      }
+      return Promise.resolve(response.data)
     }
     else {
-      useUserStore().logout()
+      return Promise.reject(response.data)
     }
-    return Promise.resolve(response.data)
   },
   async (error) => {
     // 获取请求配置
     const config = error.config
-
     // 如果配置不存在或未启用重试，则直接处理错误
     if (!config || !config.retry) {
       return handleError(error)
     }
-
     // 设置重试次数
     config.retryCount = config.retryCount || 0
-
     // 判断是否超过重试次数
     if (config.retryCount >= MAX_RETRY_COUNT) {
       return handleError(error)
     }
-
     // 重试次数自增
     config.retryCount += 1
-
     // 延迟重试
     await new Promise(resolve => setTimeout(resolve, RETRY_DELAY))
-
     // 重新发起请求
     return api(config)
   },
